784cb9a68061e3a37ec298daa61177b0632a0d98
mucit <mucithan@gmail.com>
2017-12-27 23:27:44 +0300

    he

---
+++ wp-login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="tr-TR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="tr-TR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Yemek Zevki &lsaquo; Giriş</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.4' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/wp-content/uploads/2017/10/cropped-yzhxx-1-32x32.gif" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2017/10/cropped-yzhxx-1-192x192.gif" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2017/10/cropped-yzhxx-1-180x180.gif" />
+<meta name="msapplication-TileImage" content="http://yemekzevki.net/wp-content/uploads/2017/10/cropped-yzhxx-1-270x270.gif" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-tr-tr">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="WordPress&#039;in desteğiyle" tabindex="-1">Yemek Zevki</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Kullanıcı adı ya da e-posta adresi<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Parola<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Beni hatırla</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
+		<input type="hidden" name="redirect_to" value="http://yemekzevki.net/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Yemek Zevki sitesine geri dön</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
